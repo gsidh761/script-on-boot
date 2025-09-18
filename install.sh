4302b786641d3e5ee7f86e5d7bcd624b52fdcc64
Gureet Sidhu <gsidh761@mtroyal.ca>
2025-09-18 12:10:19 -0600

    Adds variables and basic backup function...

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
 
 # This script should backup the ~/.profile file and add the custom script to the bottom of the file. This should support using $1 to get the script
+
+PROFILE="~/.profile"
+SCRIPT="$1"
+BACKUPDIR="~/.script-on-boot/"
+
+mkdir "$BACKUPDIR" 
+cp "$PROFILE" "$BACKUPDIR"
+
