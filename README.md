b2df62dab47d9d32b49525cdf81c58ca6c2fddfc
Gureet Sidhu <gsidh761@mtroyal.ca>
2025-09-18 13:16:03 -0600

    Fix...

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Script on Boot
 **Run shell scripts on login without needing root...**
 
-**This script will backup the existing ~/.profile and add the script to the bottom of the file**
+**This script will backup the existing ~/.bash_profile and add the script to the bottom of the file**
--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 
 # This script should backup the ~/.profile file and add the custom script to the bottom of the file. This should support using $1 to get the script
 
-PROFILE="~/.profile"
+PROFILE="~/.bash_profile"
 SCRIPT="$1"
-BACKUPDIR="~/.script-on-boot/"
+BACKUPDIR="~/.scripts"
 
-mkdir "$BACKUPDIR" 
-echo "Copying old ~/.profile to $BACKUPDIR"
+mkdir -p "$BACKUPDIR" 
+echo "Copying old $PROFILE to $BACKUPDIR"
 cp "$PROFILE" "$BACKUPDIR"
-echo "Modifiying ~/.profile file to add script..."
+echo "Modifiying $PROFILE file to add script..."
+if ! grep -q "$SCRIPT" "$PROFILE"; then
+            cat >> "sh $SCRIPT" <<EOF
+
+EOF
 echo "Done..."
