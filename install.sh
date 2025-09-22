26dbf3e4dd723649170cac5833f120d4be16551f
Gureet Sidhu <gsidh761@mtroyal.ca>
2025-09-22 09:07:10 -0600

    Script now works...

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 
 # This script should backup the ~/.profile file and add the custom script to the bottom of the file. This should support using $1 to get the script
 
-PROFILE="~/.bash_profile"
+PROFILE="$HOME/.bash_profile"
 SCRIPT="$1"
-BACKUPDIR="~/.scripts"
+BACKUPDIR="$HOME/.scripts"
 
 mkdir -p "$BACKUPDIR" 
 echo "Copying old $PROFILE to $BACKUPDIR"
 cp "$PROFILE" "$BACKUPDIR"
-echo "Modifiying $PROFILE file to add script..."
-if ! grep -q "$SCRIPT" "$PROFILE"; then
-            cat >> "sh $SCRIPT" <<EOF
-
+echo "Trying to Modifiy $PROFILE file to add script..."
+if cat "$PROFILE" | cat "$SCRIPT">/dev/null 2>&1; then
+        echo "Editing File..."
+        echo "chmod +x $SCRIPT && $SCRIPT" >> "$PROFILE" <<EOF
 EOF
+fi
 echo "Done..."
